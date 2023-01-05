bf7928882a462e553550f0d232cc8e54196bba90
AtharvaBanasure <atharvabanasure@gmail.com>
2023-01-06 00:39:43 +0530

    Create MicrosoftQue13.java

---
+++ MicrosoftQue13.java
@@ -0,0 +1,9 @@
+public class MicrosoftQue13 {
+        public double nthPersonGetsNthSeat(int n) {
+            if(n==1){
+                return 1;
+            }else{
+                return 0.5;
+            }
+        }
+}
