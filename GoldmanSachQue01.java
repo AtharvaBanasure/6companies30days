8ad1734cc2ed87fac8037d556c3ed873af5bf6af
AtharvaBanasure <atharvabanasure@gmail.com>
2023-01-06 17:25:57 +0530

    Update GoldmanSachQue01.java

---
+++ GoldmanSachQue01.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxPoints(int[][] points) {
+        
+        int m=points.length;
+        if(points.length==1){
+            return 1;
+        }else if(points.length==2){
+            return 2;
+        }
+            int max=0;
+            
+            for(int i=0;i<m;i++){
+                HashMap<Double,Integer>map=new HashMap<>();
+                for(int j=0;j<m;j++){
+                    if(i==j){
+                        continue;
+                    }
+                    int x1=points[i][0];
+                    int x2=points[j][0];
+                    int y1=points[i][1];
+                    int y2=points[j][1];
+                    System.out.print(x1);
+                    System.out.print(y1);
+                    System.out.println();
+                    System.out.print(x2);
+                    
+                    System.out.print(y2);
+                    System.out.println();
+                    double k=(double)(y2-y1)/(double)(x2-x1);
+                   
+                    map.put(k,map.getOrDefault(k,1) + 1);
+                    
+                    max=Math.max(max,map.get(k));
+                }
+            }
+            
+           
+                
+            
+        return max;
+    }
+}
